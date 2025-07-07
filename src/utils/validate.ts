b1e3e639815261532e331a64561582c20a2bf8df
Parth Dhemeliya <parthd@mindfiresolutions.com>
2025-07-07 12:13:13 +0530

    fix: validator contructor error

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,23 +1,23 @@
-import { z } from 'zod';
+import { z, ZodSchema, ZodError } from 'zod';
 import { Request, Response, NextFunction } from 'express';
 import { AppError } from './error/AppError';
 
-export const validate =
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (schema: z.ZodSchema<any>) =>
-    (req: Request, res: Response, next: NextFunction) => {
-      try {
-        schema.parse(req.body);
-        next();
-      } catch (error) {
-        if (error instanceof z.ZodError) {
-          const formattedErrors = error.issues.map((issue) => ({
-            field: issue.path.join('.'),
-            message: issue.message,
-          }));
-          return next(new AppError('Validation error', 400, formattedErrors));
-        }
+export const validate = (schema: ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const formattedErrors = err.issues.map((issue) => ({
+          field: issue.path.join('.'),
+          message: issue.message,
+        }));
 
-        return next(new AppError('Unknown validation error', 400));
+        return next(new AppError('Validation error', 400, formattedErrors));
       }
-    };
+
+      return next(new AppError('Unknown validation error', 400));
+    }
+  };
+};
